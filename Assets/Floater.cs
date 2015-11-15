01039720e42beb830bbbe5598bc75465fc500f98
sahlal <sahool.90@gmail.com>
2015-11-14 21:01:20 -0400

    Floating Objects Implementation

---
+++ Assets/Floater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < 2.68) {                                                  
+			Debug.Log(gameObject.transform.position.y +" AND "+ gameObject.GetComponent<Rigidbody> ().velocity);
+		    gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.up * 8.5f);
+		
+		
+
+
+		}
+	}
+}
