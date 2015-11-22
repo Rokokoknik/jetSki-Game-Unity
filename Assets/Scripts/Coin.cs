9a7a3cf11f7cc7c0176a01e898e086ddee4cadce
sahlal <sahool.90@gmail.com>
2015-11-22 17:11:24 -0400

    Implemented Features in issues #9 #11 #15

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+	public float rotationSpeed = 10f;
+	public int scoreValue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.GetComponent<PlayerStats>().AddScore(scoreValue);
+			gameObject.GetComponent<AudioSource>().Play();
+			gameObject.GetComponent<MeshRenderer>().enabled=false;
+			Destroy(gameObject,2);
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	private int currentScore;
+
+	// Use this for initialization
+	void Start () {
+		this.currentScore = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (currentScore);
+	}
+
+	public void AddScore(int score){
+		this.currentScore += score;
+	}
+}
