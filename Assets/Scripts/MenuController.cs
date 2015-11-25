b2c8c03bd91028e1a7f550c6fa8d7440b5b1a464
sahlal <sahool.90@gmail.com>
2015-11-25 17:24:39 -0400

    Implemented Main Menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public GameObject optionsPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadLevel1(){
+		Application.LoadLevel ("Level 1");
+	}
+
+	public void ToggleOptionsPanel(){
+		if(optionsPanel.activeSelf==true){
+			optionsPanel.SetActive (false);
+		}
+		else if(optionsPanel.activeSelf==false){
+			optionsPanel.SetActive (true);
+		}
+	
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
