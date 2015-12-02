c69fe34f4e7f4b57b777fb0e39304f691f28606d
Gerald <Rokokoknik@gmail.com>
2015-12-02 09:43:59 -0400

    Updated timer/Ramp Collider
    Not a prefab yet, but the ramp in the level 1 demo is currently working
    correctly. The Countdown timer has a paused boolean value in order to
    pause the timer when menu is opened.

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -5,29 +5,48 @@ using UnityEngine.UI;
 public class CountdownTimer : MonoBehaviour {
 
 	public float timer;
+	private float initialtimer;
 	private float mtimer;
+	private bool paused = false;
+
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<Text>().color = Color.green;
+		initialtimer = timer;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		timer -= Time.deltaTime;
-		mtimer = timer * 100;
-		mtimer -= (Time.deltaTime * 100);
-		int minutes = Mathf.FloorToInt (timer / 60f);
-		int seconds = Mathf.FloorToInt (timer - minutes * 60);
-		int milliseconds = Mathf.FloorToInt (mtimer % 100);
-		string formattime = string.Format ("{0:00}:{1:00};{2:00}", minutes, seconds, milliseconds);
+		//A boolean value to detect if the game is paused, stops timer
+		if (!paused) {
+			//Seconds based timer
+			timer -= Time.deltaTime;
+			//milliseconds based timer
+			mtimer = timer * 100;
+			//Makes it take off a millisecond 100 times a second
+			mtimer -= (Time.deltaTime * 100);
+			int minutes = Mathf.FloorToInt (timer / 60f);
+			int seconds = Mathf.FloorToInt (timer - minutes * 60);
+			int milliseconds = Mathf.FloorToInt (mtimer % 100);
+			//Formats the values for use on screen
+			string formattime = string.Format ("{0:00}:{1:00};{2:00}", minutes, seconds, milliseconds);
+			//Sets the on screen Text element to the current value;
+			gameObject.GetComponent<Text>().text = formattime;
 
-		gameObject.GetComponent<Text>().text = formattime;
+		}
 		//if (timer > 299f)
 		//	gameObject.GetComponent<Text>().color = Color.green;
-		if (Mathf.Ceil (timer) == 60)
+		if (Mathf.Ceil (timer) == Mathf.Floor (initialtimer/2))
 			gameObject.GetComponent<Text>().color = Color.yellow;
-		if (Mathf.Ceil (timer) == 20)
+		if (Mathf.Ceil (timer) == Mathf.Floor (initialtimer/5))
 			gameObject.GetComponent<Text>().color = Color.red;
-
+		if (Mathf.Ceil (timer) == 0.0f) {
+			gameObject.GetComponent<Text> ().text = "00:00;00";
+			paused = true;
+		}
+	}
+	void setPaused (bool p)
+	{
+		paused = p;
 	}
 }
---
+++ Assets/Scripts/RampCollide.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RampCollide : MonoBehaviour {
+
+	public float thrust;
+	//public float angle;
+
+	void OnCollisionEnter (Collision collisionInfo) {
+		collisionInfo.rigidbody.AddRelativeForce ((Vector3.forward + Vector3.up) * thrust); 
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
