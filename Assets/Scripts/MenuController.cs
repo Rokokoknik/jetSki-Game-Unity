2ec48b1c91c6f00be32098a81dca1a4d843776cf
sahlal <sahool.90@gmail.com>
2015-12-05 07:18:23 -0400

    Added Volume & FOV sliders to Options in main menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
 	public GameObject optionsPanel;
+	public GameObject soundSlider;
+	public GameObject fieldOfViewSlider;
+	public static float fieldOfView=60;
 
 	// Use this for initialization
 	void Start () {
-	
+		soundSlider.GetComponent<Slider> ().value = AudioListener.volume;
+		fieldOfViewSlider.GetComponent<Slider> ().value = fieldOfView;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	//	Debug.Log(AudioListener.volume);
+	//	Debug.Log(GameObject.FindGameObjectsWithTag ("MainCamera") [0].gameObject.GetComponent<Camera> ().fieldOfView);
 	}
 
 	public void LoadLevel1(){
@@ -30,5 +36,14 @@ public class MenuController : MonoBehaviour {
 
 	public void ExitGame(){
 		Application.Quit ();
+	}
+
+	public void ChangeSoundVolume(){
+		AudioListener.volume = soundSlider.GetComponent<Slider>().value;
+	}
+
+	public void ChangeFieldOfView(){
+		fieldOfView = fieldOfViewSlider.GetComponent<Slider> ().value;
+		GameObject.FindGameObjectsWithTag ("MainCamera") [0].gameObject.GetComponent<Camera> ().fieldOfView = fieldOfViewSlider.GetComponent<Slider> ().value;
 	}
 }
