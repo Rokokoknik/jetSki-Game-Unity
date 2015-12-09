00926f3bd96e3be612fa25500a8a37638581d6b1
sahlal <sahool.90@gmail.com>
2015-12-09 07:31:42 -0400

    Pause Camera Fix

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -12,8 +12,6 @@ public class PauseMenu : MonoBehaviour {
 	public float rotationSpeed;
 	// Use this for initialization
 	void Start () {
-	//	cameraOriginalLocation = cameraRotation.transform.position;
-	//	cameraOriginalRotation = cameraRotation.transform.rotation;
 		rotationSpeed = 0;
 	}
 	
